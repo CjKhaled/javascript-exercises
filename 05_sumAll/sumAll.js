dc98bbc6fe064c5efd7f9f1b5db660d4413b892a
Cj Alexander <clauvinsalexander@gmail.com>
2023-11-04 17:19:31 -0400

    Finished exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let totalSum = 0;
+  const checkHighest = new Array(start, end);
+  if (
+    typeof checkHighest[0] !== "number" ||
+    typeof checkHighest[1] !== "number"
+  ) {
+    return "ERROR";
+  }
+  start = Math.min(...checkHighest);
+  end = Math.max(...checkHighest);
+  for (let i = start; i <= end; i++) {
+    totalSum += i;
+  }
+  if (totalSum < 0) {
+    return "ERROR";
+  } else {
+    return totalSum;
+  }
 };
 
 // Do not edit below this line
