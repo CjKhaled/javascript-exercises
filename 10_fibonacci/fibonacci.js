8e0951870a96f0a7c05ea4f824b9f73c0d7e1edd
Cj Alexander <clauvinsalexander@gmail.com>
2023-11-07 09:18:11 -0500

    Finished final three exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS"
+    } else if (!Number.isInteger(number)) {
+        number = parseInt(number)
+    }
+    let startOfFibonacci = [1, 1]
+    const trueRange = number - 2
+    let firstIndex = 0
+    let secondIndex = 1
+    for (let i= 0; i < trueRange; i++) {
+        startOfFibonacci.push(startOfFibonacci[firstIndex] + startOfFibonacci[secondIndex])
+        secondIndex++
+        firstIndex++
+    }
+    return startOfFibonacci[startOfFibonacci.length - 1]
 };
 
 // Do not edit below this line
