c3e4f5f3ebc581dc48f8af1c2687d628b40a68b0
Cj Alexander <clauvinsalexander@gmail.com>
2023-11-07 08:25:00 -0500

    Finished exercises 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = array.reduce((total, number) => total + number, 0)
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = array.reduce((total, number) => total * number, 1)
+  return total
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let total = 1
+  for (let i = 1; i < number + 1; i++){
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanedString = [...string].filter((letter) => letter !== " " &&
+    letter !== "." && letter !== "," && letter !== "!").map((letter) => 
+    letter.toLowerCase())
+    // Make sure the original array isn't altered
+    const stringToCompare= [...cleanedString].reverse()
+    
+    for (let i = 0; i < cleanedString.length; i++) {
+        if (cleanedString[i] !== stringToCompare[i]) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
